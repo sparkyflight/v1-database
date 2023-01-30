a24fefe8d0b6f8a8110ff90005245831f7bec62c
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-30 14:11:23 -0600

    Update handler.js

--- handler.js
+++ handler.js
@@ -4,6 +4,6 @@ const fs = require("fs");
 const logger = require("./logger");
 
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://select:Nineteen19@nightmareproject.5en4i6u.mongodb.net/?retryWrites=true&w=majority").then(() => {
+mongoose.connect("mongodb+srv://select:PPA10082@nightmareproject.5en4i6u.mongodb.net/?retryWrites=true&w=majority").then(() => {
     logger.success("Mongoose", "Connected!");
 });
