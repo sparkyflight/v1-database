fd03ab4025e559700634b3fc4e743348f97d736c
selectdev <66577268+selectdev@users.noreply.github.com>
2023-04-09 14:08:23 -0500

    Update polls.js

--- schemas/polls.js
+++ schemas/polls.js
@@ -3,7 +3,7 @@ const { Schema } = require("mongoose");
 const schema = new Schema({
 	UserID: String,
 	CreatedAt: Date,
-        ExpirationDate: Date
+        ExpirationDate: Date,
 	Question: String,
         Description: String,
         Options: Array,
