3085904c90a96cc4a627d71b6e593ba009e4cf9a
selectdev <66577268+selectdev@users.noreply.github.com>
2023-04-09 14:08:06 -0500

    Create polls.js

---
+++ schemas/polls.js
@@ -0,0 +1,15 @@
+const { Schema } = require("mongoose");
+
+const schema = new Schema({
+	UserID: String,
+	CreatedAt: Date,
+        ExpirationDate: Date
+	Question: String,
+        Description: String,
+        Options: Array,
+});
+
+module.exports = {
+	name: "poll",
+	schema: schema,
+};
